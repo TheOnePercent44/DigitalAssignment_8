6a8bbff18cf865722c78d4b79c90336a7c67bb8d
TheOnePercent44 <gamemakervx@gmail.com>
2015-04-08 20:11:40 -0400

    Text labels

--- js/DA8_Game.js
+++ js/DA8_Game.js
@@ -100,6 +100,16 @@ Indirect.Game.prototype = {
 		yaggroL = this.game.add.text(576, 924, '0', 0);
 		
 		setNums();
+		
+		this.game.add.text(100, 820, 'Red fears Yellow', {font: "15px Arial", fill: "#ffffff", align: "left"});
+		this.game.add.text(100, 872, 'Red fears Blue', {font: "15px Arial", fill: "#ffffff", align: "left"});
+		this.game.add.text(100, 924, 'Red Aggression', {font: "15px Arial", fill: "#ffffff", align: "left"});
+		this.game.add.text(292, 820, 'Blue fears Red', {font: "15px Arial", fill: "#ffffff", align: "left"});
+		this.game.add.text(292, 872, 'Blue fears Yellow', {font: "15px Arial", fill: "#ffffff", align: "left"});
+		this.game.add.text(292, 924, 'Blue Aggression', {font: "15px Arial", fill: "#ffffff", align: "left"});
+		this.game.add.text(484, 820, 'Yellow fears Red', {font: "15px Arial", fill: "#ffffff", align: "left"});
+		this.game.add.text(484, 872, 'Yellow fears Blue', {font: "15px Arial", fill: "#ffffff", align: "left"});
+		this.game.add.text(484, 924, 'Yellow Aggression', {font: "15px Arial", fill: "#ffffff", align: "left"});
 	/////////////////////////////////////////////////////////////////////////////////
 		this.game.input.keyboard.onUpCallback = keyReleased;
     },
